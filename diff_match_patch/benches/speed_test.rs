75caa6858f6fdd27af5c04bf83bc2220c9e45015
Evgeny Shurakov <eshurakov@users.noreply.github.com>
2021-04-15 18:28:41 +0200

    Adding a speedtest benchmark

---
+++ diff_match_patch/benches/speed_test.rs
@@ -0,0 +1,19 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use diff_match_patch::Dmp;
+
+use std::fs;
+
+fn test(text1: &str, text2: &str) {
+    let dmp = Dmp::new();
+    dmp.diff_main(text1, text2, true);
+}
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let file1 = fs::read_to_string("./benches/Speedtest1.txt").unwrap();
+    let file2 = fs::read_to_string("./benches/Speedtest2.txt").unwrap();
+
+    c.bench_function("dmp-speedtest", |b| b.iter(|| test(black_box(&file1), black_box(&file2)) ));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
